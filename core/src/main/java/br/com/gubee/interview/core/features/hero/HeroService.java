205f5902ff0138fd9e212b4be72fea6661439a08
Adriel Felix <adrielandrade3@gmail.com>
2022-08-26 02:23:15 -0300

    create hero bug fix

--- core/src/main/java/br/com/gubee/interview/core/features/hero/HeroService.java
+++ core/src/main/java/br/com/gubee/interview/core/features/hero/HeroService.java
@@ -1,6 +1,8 @@
 package br.com.gubee.interview.core.features.hero;
 
+import br.com.gubee.interview.core.features.powerstats.PowerStatsRepository;
 import br.com.gubee.interview.model.Hero;
+import br.com.gubee.interview.model.PowerStats;
 import br.com.gubee.interview.model.request.CreateHeroRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,8 +16,11 @@ public class HeroService {
 
     private final HeroRepository heroRepository;
 
+    private final PowerStatsRepository powerStatsRepository;
+
     @Transactional
     public UUID create(CreateHeroRequest createHeroRequest) {
-        return heroRepository.create(new Hero(createHeroRequest, null));
+        final UUID powerStatsId = powerStatsRepository.create(new PowerStats(createHeroRequest));
+        return heroRepository.create(new Hero(createHeroRequest, powerStatsId));
     }
 }
